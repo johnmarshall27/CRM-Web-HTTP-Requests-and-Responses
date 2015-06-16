8e80645a9e9bf7a9b1d4712793804a3560f1335c
John Marshall <john.marshall4997@hotmail.com>
2015-06-16 15:17:43 -0400

    It all works (except for the id part)

--- crm.rb
+++ crm.rb
@@ -11,6 +11,16 @@ get '/' do
 end
 
 get '/contacts' do
-  erb :contacts
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
+
+get'/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  puts params
 end
 
