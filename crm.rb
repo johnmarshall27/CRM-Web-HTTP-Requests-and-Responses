b015fe7a8101d4c825626ca12366f5700da09b2f
John Marshall <john.marshall4997@hotmail.com>
2015-06-16 14:30:49 -0400

    Working display all contacts (issues with id as it is not defined w/o global variable

--- crm.rb
+++ crm.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require_relative'contact'
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -7,5 +8,10 @@ get '/' do
 end
 
 get '/contacts' do
+  @contacts = []
+  @contacts << Contact.new("john", "Marshall","john.marshall@ucc.on.ca", "Developer")
+  @contacts << Contact.new("john1", "Marshall1","john.marshall1@ucc.on.ca", "Developer1")
+  @contacts << Contact.new("john2", "Marshall2","john.marshall2@ucc.on.ca", "Developer2")
   erb :contacts
 end
+
